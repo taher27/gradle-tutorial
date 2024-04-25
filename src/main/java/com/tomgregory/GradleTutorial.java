2e71726ae44416d53a329852970db1092468c717
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-04-25 13:47:30 +0530

    add two more functions

--- src/main/java/com/tomgregory/GradleTutorial.java
+++ src/main/java/com/tomgregory/GradleTutorial.java
@@ -4,4 +4,10 @@ public class GradleTutorial {
     public static void main(String[] args) {
         System.out.println("Gradle 4tw!");
     }
+    public int addTwoNumbers(int a,int b){
+        return a+b;
+    }
+    public int multiplyTwoNumbers(int a,int b){
+        return a*b;
+    }
 }
